71469dd30968b0bec7e839883e2073755c549014
jon2-vera-cruz <jon2_vera_cruz@hotmail.com>
2020-10-16 23:08:20 +0000

    Design-1.0.9 - introduced text to speech synthesizers for scoring

--- assets/js/score.js
+++ assets/js/score.js
@@ -1,25 +1,64 @@
 var teamAScore = 0;
 var teamBScore = 0;
+var serviceOver = 2;
 function incrementScore(scoreSide) {
+    if (teamAScore == 0 && teamBScore == 0) {
+        /*let speakThisMsg = new SpeechSynthesisUtterance();
+        speakThisMsg = "Player B to server Player C";
+        window.speechSynthesis.speak(new SpeechSynthesisUtterance(speakThisMsg));
+        speakThisMsg = "love all";
+        window.speechSynthesis.speak(new SpeechSynthesisUtterance(speakThisMsg));
+        speakThisMsg = "play";
+        window.speechSynthesis.speak(new SpeechSynthesisUtterance(speakThisMsg));*/
+    };
+
+    //this needs to move when user press Start Match button
     $(".player-drop-down-toggle").prop("disabled", true);
+
+
     if (scoreSide == 'left') {
         teamAScore++;
         if (teamAScore % 2 == 0) {
-            //showHideShuttle(`${scoreSide}-court-right-side-player`);
-            showHideShuttle(scoreSide, 'right');
+            showHideShuttle(`${scoreSide}`, 'right');
         } else {
-            //showHideShuttle(`${scoreSide}-court-left-side-player`);
-            showHideShuttle(scoreSide, 'left');
+            showHideShuttle(`${scoreSide}`, 'left');
         };
+        if (scoreSide == 'left' && serviceOver == 'left') {
+            speakThisMsg = "Service Over";
+            window.speechSynthesis.speak(new SpeechSynthesisUtterance(speakThisMsg));
+        }
+        speakThisMsg = "Score";
+        window.speechSynthesis.speak(new SpeechSynthesisUtterance(speakThisMsg));
+        if (teamAScore == teamBScore) {
+            window.speechSynthesis.speak(new SpeechSynthesisUtterance(teamAScore));   
+            speakThisMsg = "all";
+            window.speechSynthesis.speak(new SpeechSynthesisUtterance(speakThisMsg));
+        } else {
+            window.speechSynthesis.speak(new SpeechSynthesisUtterance(teamAScore));   
+            window.speechSynthesis.speak(new SpeechSynthesisUtterance(teamBScore));
+        };
+        serviceOver = 'right';
     } else if (scoreSide == 'right') {
         teamBScore++;
         if (teamBScore % 2 == 0) {
-            //showHideShuttle(`${scoreSide}-court-right-side-player`);
-            showHideShuttle(scoreSide, 'right');
+            showHideShuttle(`${scoreSide}`, 'right');
         } else {
-            //showHideShuttle(`${scoreSide}-court-left-side-player`);
-            showHideShuttle(scoreSide, 'left');
+            showHideShuttle(`${scoreSide}`, 'left');
         };
+        if (scoreSide == 'right' && serviceOver == 'right') {
+            speakThisMsg = "Service Over";
+            window.speechSynthesis.speak(new SpeechSynthesisUtterance(speakThisMsg));
+        }
+        speakThisMsg = "Score";
+        window.speechSynthesis.speak(new SpeechSynthesisUtterance(speakThisMsg));
+        if (teamBScore == teamAScore) {
+            window.speechSynthesis.speak(new SpeechSynthesisUtterance(teamBScore));   
+            speakThisMsg = "all";
+            window.speechSynthesis.speak(new SpeechSynthesisUtterance(speakThisMsg));
+        } else {
+            window.speechSynthesis.speak(new SpeechSynthesisUtterance(teamBScore));   
+            window.speechSynthesis.speak(new SpeechSynthesisUtterance(teamAScore));
+        };
+        serviceOver = 'left';
     };
-    console.log(teamAScore, teamBScore);
-}
+};
--- assets/js/shuttle.js
+++ assets/js/shuttle.js
@@ -1,36 +1,10 @@
 function showHideShuttle(courtSide, serviceSide) {
     // Hide all the shuttlecock image first
     $(`#left-court-left-side-shuttle`).hide();
-    $(`#left-court-right-side-shuttle`.hide();
+    $(`#left-court-right-side-shuttle`).hide();
     $(`#right-court-left-side-shuttle`).hide();
-    $(`#right-court-right-side-shuttle`.hide();
+    $(`#right-court-right-side-shuttle`).hide();
     // then show the player who will serve    
     $(`#${courtSide}-court-${serviceSide}-side-shuttle`).show();
-
-    /*switch (servicePlayer) {
-        case 'left-court-left-side-player':
-            $('#left-court-left-side-shuttle').show();
-            $('#left-court-right-side-shuttle').hide();
-            $('#right-court-left-side-shuttle').hide();
-            $('#right-court-right-side-shuttle').hide();
-            break;
-        case 'left-court-right-side-player':
-            $('#left-court-left-side-shuttle').hide();
-            $('#left-court-right-side-shuttle').show();
-            $('#right-court-left-side-shuttle').hide();
-            $('#right-court-right-side-shuttle').hide();
-            break;
-        case 'right-court-left-side-player':
-            $('#left-court-left-side-shuttle').hide();
-            $('#left-court-right-side-shuttle').hide();
-            $('#right-court-left-side-shuttle').show();
-            $('#right-court-right-side-shuttle').hide();
-            break;
-        case 'right-court-right-side-player':
-            $('#left-court-left-side-shuttle').hide();
-            $('#left-court-right-side-shuttle').hide();
-            $('#right-court-left-side-shuttle').hide();
-            $('#right-court-right-side-shuttle').show();
-            break;
-    };*/
+    console.log(teamAScore, teamBScore, courtSide, serviceSide);
 };
