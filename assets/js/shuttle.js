f0fa94351e2987b2f797aad6bb5f24fec28588e9
jon2-vera-cruz <jon2_vera_cruz@hotmail.com>
2020-10-16 16:34:51 +0000

    Added shuttle.js to manipulate the shuttlecock images in the court

---
+++ assets/js/shuttle.js
@@ -0,0 +1,7 @@
+function showHideShuttle(elementID) {
+  if $(elementID).style.display === "none") {
+    e$(elementID).style.display = "block";
+  } else {
+    $(elementID).style.display = "none";
+  }
+}
