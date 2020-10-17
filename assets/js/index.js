50fa4515f485a4d216fcf94511bd0d6d872271ca
jon2-vera-cruz <jon2_vera_cruz@hotmail.com>
2020-10-17 21:37:58 +0000

    Design-1.0.15 - Added the Server Side option to decide which court side will serve the shuttlecock initially

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,9 @@
 $("document").ready(function () {
     $(`.left-scorer`).attr("disabled", "disabled");
     $(`.right-scorer`).attr("disabled", "disabled");
+    $(`.left-court-left-player-container`).hide();
+    $(`.team-a-player-1`).hide();
+    $(`.right-court-left-player-container`).hide();
+    $(`.team-b-player-1`).hide();
+    $(`#left-court-right-side-shuttle`).show();
 });
--- assets/js/player.js
+++ assets/js/player.js
@@ -2,3 +2,17 @@ $('a').click(function(player) {
     console.log($(this).text().toLowerCase());
     $(this).parent().parent().prev().children('img').attr("src", `assets/images/${$(this).text().toLowerCase()}-player.png`);
 });
+
+function showHidePlayers(option) {
+    if(option == 'hide') {
+        $(`.left-court-left-player-container`).hide();
+        $(`.team-a-player-1`).hide();
+        $(`.right-court-left-player-container`).hide();
+        $(`.team-b-player-1`).hide();
+    } else {
+        $(`.left-court-left-player-container`).show();
+        $(`.team-a-player-1`).show();
+        $(`.right-court-left-player-container`).show();
+        $(`.team-b-player-1`).show();
+    }
+}
