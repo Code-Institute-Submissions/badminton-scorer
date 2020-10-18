c32c80c4131468c552b959ba30c9b70bd09bd20c
jon2-vera-cruz <jon2_vera_cruz@hotmail.com>
2020-10-18 20:16:13 +0000

    Design-Dev-1.0.2 - continue adding elements in the setup menu settings

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,3 +7,32 @@ $("document").ready(function () {
     $(`.team-b-player-1`).hide();
     $(`#left-court-right-side-shuttle`).show();
 });
+// Source https://stackoverflow.com/questions/469357/html-text-input-allow-only-numeric-input
+function validate(interval, evt) {
+  var theEvent = evt || window.event;
+
+  // Handle paste
+  if (theEvent.type === 'paste') {
+      key = event.clipboardData.getData('text/plain');
+  } else {
+  // Handle key press
+      var key = theEvent.keyCode || theEvent.which;
+      key = String.fromCharCode(key);
+  }
+  var regex = /[0-9]|\./;
+  if( !regex.test(key) ) {
+    theEvent.returnValue = false;
+    if(theEvent.preventDefault) theEvent.preventDefault();
+  }
+};
+
+$("#mid-interval-seconds").keyup(function() {
+  if($('#mid-interval-seconds').val()>360 ){
+      $("#mid-interval-seconds" ).val("");
+  }
+});
+$("#full-interval-seconds").keyup(function() {
+  if($('#full-interval-seconds').val()>360 ){
+      $("#full-interval-seconds" ).val("");
+  }
+});
