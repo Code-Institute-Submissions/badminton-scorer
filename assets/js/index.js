f22ec80f778fd7543b05065813ca5f8036e23d49
jon2-vera-cruz <jon2_vera_cruz@hotmail.com>
2020-10-19 01:04:39 +0000

    Design-Dev-1.0.8 added automated computation and adjustment of the player names elements.

--- assets/js/index.js
+++ assets/js/index.js
@@ -40,12 +40,51 @@ $("#full-interval-seconds").keyup(function() {
 
 // This will automatically position the Setup Menu element to the center if the window is resize.
 function centerMenuSetting() {
-    $(`.setup-menu-option`).css("left", ($(`.master-container`).width()) / 2 - ($(`.setup-menu-option`).width() / 2));
+    $(`.setup-menu-option`).css("left", ($(`.master-container`).width() / 2) - ($(`.setup-menu-option`).width() / 2));
+    $(`.setup-menu-option`).css("top", ($(`.master-container`).height() / 2) - $(`.setup-menu-option`).css.height());
     $(`.left-court-shuttles`).css("left", ($(".left-court-left-service-area").width() / 2) + $(`.left-court-shuttles`).width() / 2);
     $(`.right-court-shuttles`).css("left", ($(".right-court-left-service-area").width() / 2) - $(`.right-court-shuttles`).width() / 2);
-}
+    $(`.team-player`).width($(`.left-court-left-service-area`).width() * 0.5);
+    $(`#team-a-player-1`).css("top", ($(`.master-container`).height() / 2) - $(`#team-a-player-1`).height() * 4);
+    $(`#team-b-player-2`).css("top", ($(`.master-container`).height() / 2) - $(`#team-a-player-2`).height() * 4);
+    $(`#team-a-player-2`).css("top", ($(`.master-container`).height()) - $(`#team-a-player-1`).height() * 6);
+    $(`#team-b-player-1`).css("top", ($(`.master-container`).height()) - $(`#team-a-player-2`).height() * 6);
+};
 
 // Check for orientation changes
 window.addEventListener("orientationchange", function() {
         centerMenuSetting();
 }, false);
+
+function gameStart() {
+    $(`.left-scorer`).attr("disabled", "enabled");
+    $(`.right-scorer`).attr("disabled", "enabled");
+    $(".player-drop-down-toggle").prop("disabled", true);
+    $(`.setup-menu-option`).hide();
+    startGame = true;
+
+    //Instantiate each player with names and icon color
+    let playerA = new Player();
+    let playerB = new Player();
+    let playerC = new Player();
+    let playerD = new Player();
+
+    //From this block of codes need to move when user press Start Match button
+    if (teamAScore == 0 && teamBScore == 0) {
+        let speakThisMsg = new SpeechSynthesisUtterance();
+        speakThisMsg = "Player B to server Player C";
+        window.speechSynthesis.speak(new SpeechSynthesisUtterance(speakThisMsg));
+        speakThisMsg = "love all";
+        window.speechSynthesis.speak(new SpeechSynthesisUtterance(speakThisMsg));
+        speakThisMsg = "play";
+        window.speechSynthesis.speak(new SpeechSynthesisUtterance(speakThisMsg));
+    };
+
+    //To this block of codes need to move when user press Start Match button
+    return;
+};
+
+function Player (name, color) {
+    this.name = name;
+    this.color = color;
+};
--- assets/js/score.js
+++ assets/js/score.js
@@ -1,98 +1,80 @@
-//$("document").ready(function () {
-    var teamAScore = 0;
-    var teamBScore = 0;
-    var serviceOver = 2;
-    var pending = false;
-    var startGame = false;
-    function incrementScoreDelay(scoreSide) {
-        $(`.left-scorer`).prop("disabled", true);
-        $('.left-scorer').off('mouseenter mouseleave'); 
-        if (!pending) {
-            pending = true;     
-            setTimeout(() => { 
-                incrementScorewithDelay(`${scoreSide}`);
-                pending = false;
-            }, 3000);
-        };
-        $(`.left-scorer`).prop("disabled", false);
-        $('.left-scorer').on('mouseenter mouseleave');
-        return;
-    };
-    function gameStart() {
-        $(`.left-scorer`).attr("disabled", "enabled");
-        $(`.right-scorer`).attr("disabled", "enabled");
-        $(".player-drop-down-toggle").prop("disabled", true);
-        $(`.setup-menu-option`).hide();
-        startGame = true;
-        //From this block of codes need to move when user press Start Match button
-        if (teamAScore == 0 && teamBScore == 0) {
-            let speakThisMsg = new SpeechSynthesisUtterance();
-            speakThisMsg = "Player B to server Player C";
-            window.speechSynthesis.speak(new SpeechSynthesisUtterance(speakThisMsg));
-            speakThisMsg = "love all";
-            window.speechSynthesis.speak(new SpeechSynthesisUtterance(speakThisMsg));
-            speakThisMsg = "play";
-            window.speechSynthesis.speak(new SpeechSynthesisUtterance(speakThisMsg));
-        };
+var teamAScore = 0;
+var teamBScore = 0;
+var serviceOver = 2;
+var pending = false;
+var startGame = false;
+var player = {};
 
-        //To this block of codes need to move when user press Start Match button
-        return;
-    };
-    function incrementScore(scoreSide) {
-        if(!startGame) {
-            alert("Game not started yet!");
-            console.log("Game not started yet!")
-            return;
-        };
-        if (scoreSide == 'left') {
-            teamAScore++;
-            if (teamAScore % 2 == 0) {
-                showHideShuttle(`${scoreSide}`, 'right');
-            } else {
-                showHideShuttle(`${scoreSide}`, 'left');
-            };
-            if (scoreSide == 'left' && serviceOver == 'left') {
-                speakThisMsg = "Service  Over";
-                window.speechSynthesis.speak(new SpeechSynthesisUtterance(speakThisMsg));
-            }
-            speakThisMsg = "Score";
-            window.speechSynthesis.speak(new SpeechSynthesisUtterance(speakThisMsg));
-            if (teamAScore == teamBScore) {
-                window.speechSynthesis.speak(new SpeechSynthesisUtterance(teamAScore));   
-                speakThisMsg = "all";
-                window.speechSynthesis.speak(new SpeechSynthesisUtterance(speakThisMsg));
-            } else {
-                window.speechSynthesis.speak(new SpeechSynthesisUtterance(teamAScore));   
-                window.speechSynthesis.speak(new SpeechSynthesisUtterance(teamBScore));
-            };
-            serviceOver = 'right';
-        } else if (scoreSide == 'right') {
-            teamBScore++;
-            if (teamBScore % 2 == 0) {
-                showHideShuttle(`${scoreSide}`, 'right');
-            } else {
-                showHideShuttle(`${scoreSide}`, 'left');
-            };
-            if (scoreSide == 'right' && serviceOver == 'right') {
-                speakThisMsg = "Service  Over";
-                window.speechSynthesis.speak(new SpeechSynthesisUtterance(speakThisMsg));
-            }
-            speakThisMsg = "Score";
-            window.speechSynthesis.speak(new SpeechSynthesisUtterance(speakThisMsg));
-            if (teamBScore == teamAScore) {
-                window.speechSynthesis.speak(new SpeechSynthesisUtterance(teamBScore));   
-                speakThisMsg = "all";
-                window.speechSynthesis.speak(new SpeechSynthesisUtterance(speakThisMsg));
-            } else {
-                window.speechSynthesis.speak(new SpeechSynthesisUtterance(teamBScore));   
-                window.speechSynthesis.speak(new SpeechSynthesisUtterance(teamAScore));
-            };
-            serviceOver = 'left';
-        };
-        //This will enable to +1 Div until all the following code has been executed.
-        $(`.left-scorer`).prop("disabled", false);
-        console.log('enable')
-        return;
-    };
 
-//});
+function incrementScoreDelay(scoreSide) {
+    $(`.left-scorer`).prop("disabled", true);
+    $('.left-scorer').off('mouseenter mouseleave'); 
+    if (!pending) {
+        pending = true;     
+        setTimeout(() => { 
+            incrementScorewithDelay(`${scoreSide}`);
+            pending = false;
+        }, 3000);
+    };
+    $(`.left-scorer`).prop("disabled", false);
+    $('.left-scorer').on('mouseenter mouseleave');
+    return;
+};
+
+function incrementScore(scoreSide) {
+    if(!startGame) {
+        alert("Game not started yet!");
+        console.log("Game not started yet!")
+        return;
+    };
+    if (scoreSide == 'left') {
+        teamAScore++;
+        if (teamAScore % 2 == 0) {
+            showHideShuttle(`${scoreSide}`, 'right');
+        } else {
+            showHideShuttle(`${scoreSide}`, 'left');
+        };
+        if (scoreSide == 'left' && serviceOver == 'left') {
+            speakThisMsg = "Service  Over";
+            window.speechSynthesis.speak(new SpeechSynthesisUtterance(speakThisMsg));
+        }
+        speakThisMsg = "Score";
+        window.speechSynthesis.speak(new SpeechSynthesisUtterance(speakThisMsg));
+        if (teamAScore == teamBScore) {
+            window.speechSynthesis.speak(new SpeechSynthesisUtterance(teamAScore));   
+            speakThisMsg = "all";
+            window.speechSynthesis.speak(new SpeechSynthesisUtterance(speakThisMsg));
+        } else {
+            window.speechSynthesis.speak(new SpeechSynthesisUtterance(teamAScore));   
+            window.speechSynthesis.speak(new SpeechSynthesisUtterance(teamBScore));
+        };
+        serviceOver = 'right';
+    } else if (scoreSide == 'right') {
+        teamBScore++;
+        if (teamBScore % 2 == 0) {
+            showHideShuttle(`${scoreSide}`, 'right');
+        } else {
+            showHideShuttle(`${scoreSide}`, 'left');
+        };
+        if (scoreSide == 'right' && serviceOver == 'right') {
+            speakThisMsg = "Service  Over";
+            window.speechSynthesis.speak(new SpeechSynthesisUtterance(speakThisMsg));
+        }
+        speakThisMsg = "Score";
+        window.speechSynthesis.speak(new SpeechSynthesisUtterance(speakThisMsg));
+        if (teamBScore == teamAScore) {
+            window.speechSynthesis.speak(new SpeechSynthesisUtterance(teamBScore));   
+            speakThisMsg = "all";
+            window.speechSynthesis.speak(new SpeechSynthesisUtterance(speakThisMsg));
+        } else {
+            window.speechSynthesis.speak(new SpeechSynthesisUtterance(teamBScore));   
+            window.speechSynthesis.speak(new SpeechSynthesisUtterance(teamAScore));
+        };
+        serviceOver = 'left';
+    };
+    //This will enable to +1 Div until all the following code has been executed.
+    $(`.left-scorer`).prop("disabled", false);
+    console.log('enable')
+    return;
+};
+
