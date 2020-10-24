18a216db4940623b712fb8010a2e1cb8f5c1ffe3
jon2-vera-cruz <jon2_vera_cruz@hotmail.com>
2020-10-24 16:14:28 +0000

    Design-Dev-1.0.32 Reflect score in the tallsy score board

--- assets/js/score.js
+++ assets/js/score.js
@@ -3,9 +3,10 @@ var teamBScore = 0;
 var serviceOver = 2;
 var startGame = false;
 var player = {};
-var numberOfSet = 1;
+var gameSet = 1;
 var voiceTimer = 3;
 var timeToVoiceOver = 0;
+var blnMidBreak = false;
 
 function incrementScore(scoreSide) {
     if(!startGame) {
@@ -13,7 +14,7 @@ function incrementScore(scoreSide) {
         console.log("Game not started yet!")
         return;
     };
-
+    console.log('Game On!', teamAScore, teamBScore);
     // disable the button until all have been executed
     disableElement(`.left-scorer`);
     disableElement(`.right-scorer`);
@@ -91,12 +92,54 @@ function incrementScore(scoreSide) {
         enableElement(`.left-scorer`);
         enableElement(`.right-scorer`);
     };
-    // This will enable the mid-game timer when a team reached 11 points
-    if(teamAScore == 11 || teamBScore == 11) {
+
+    // This will enable the mid-game interval timer when a team reached 11 points first
+    if ((teamAScore == 11 && teamBScore < 11) || (teamBScore == 11 || teamAScore < 11))  {
         setElementInnerHTML(`#interval-timer`, intMidIntervalBreak);
         $('#game-interval').modal('show');
         hideElement(`#close-interval`);
         intervalCountdown(intMidIntervalBreak);
+        (gameSet == 3) ? blnMidBreak = true : blnMidBreak = false;
+    };
+
+    // This will enable the full-game interval timer if one of the following condition is met
+    // 1. If a team score 21 points first and with a lead points of 2 or more (i.e. 21:19 or 19:21, or 21:7 etc)
+    // 2. if a team score is greater than 21 points with a lead points of 2 (i.e. 22:20, 20:22, 23:21 etc.)
+    // 3. if a team reaches 30 points first (i.e. 30:29 or 29:30)
+    console.log(teamAScore, teamBScore)
+    if ((teamAScore == 21 && teamBScore <= 19) || (teamBScore == 21 || teamAScore <= 19)
+        || (teamAScore > 21 && (teamAScore-teamBScore) >= 2) || (teamBScore > 21 && teamBScore - teamAScore > 2)
+        || (teamAScore == 30 || teamBScore == 30)) {
+        setElementInnerHTML(`#interval-timer`, intFullIntervalBreak);
+        $('#game-interval').modal('show');
+        hideElement(`#close-interval`);
+        intervalCountdown(intFullIntervalBreak);
+        gameSet++;
+        teamAScore = 0;
+        teamBScore = 0;
+    }
+
+    // Reflect the score on the Score Tally Board
+    switch(gameSet) {
+        case 1:
+            setElementValue(`#team-a-set-one`, teamAScore);
+            setElementValue(`#team-b-set-one`, teamBScore);
+            break;
+        case 2:
+            setElementValue(`#team-a-set-one`, teamBScore);
+            setElementValue(`#team-b-set-one`, teamAScore);
+            break;
+        case 3:
+            switch(blnMidBreak) {
+                case false:
+                    setElementValue(`#team-a-set-one`, teamAScore);
+                    setElementValue(`#team-b-set-one`, teamBScore);
+                    break;
+                case true:
+                    setElementValue(`#team-a-set-one`, teamBScore);
+                    setElementValue(`#team-b-set-one`, teamAScore);
+                    break;
+            }
     };
 };
 
